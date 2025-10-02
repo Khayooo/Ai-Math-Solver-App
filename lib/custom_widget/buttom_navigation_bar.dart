729f0c13f82cb72ba56c4ddbb8b8c465d2a704e1
MAK <mabdullahkayoo3@gmail.com>
2025-10-02 05:36:30 -0700

    refactor: Revamp custom bottom navigation bar

--- lib/custom_widget/buttom_navigation_bar.dart
+++ lib/custom_widget/buttom_navigation_bar.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import '../utils/app_strings.dart';
 import '../utils/asset_paths.dart';
 
 class BottomNavigationBarCustom extends StatelessWidget {
@@ -14,32 +15,95 @@ class BottomNavigationBarCustom extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size;
     return Container(
-      height: 70,
+      height: screenWidth.height *0.16,
+      child: Stack(children: [
 
-      decoration: BoxDecoration(
 
-        image: DecorationImage(
-          image: AssetImage(AssetPaths().buttom_navigation_bar),
-          fit: BoxFit.fill,
-        ),
-      ),
-      child: Column(
-        children: [
-          GestureDetector(child: Image.asset(AssetPaths().center_icon, height: 30,)),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 50),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                GestureDetector(child: Image.asset(AssetPaths().home_button,scale: 1.5,)),
-                GestureDetector(child: Image.asset(AssetPaths().history_button,scale: 1.5,)),
+        Positioned(
+          top: 0.0,
+        left: 0.0,
+        right: 0.0,
+        child:           GestureDetector(
+          onTap: () => onTap(2),
+          child: Container(
+            height: 70,
+            width: 70,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              color: Colors.white,
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black26,
+                  blurRadius: 6,
+                  offset: Offset(0, 3),
+                )
               ],
             ),
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Image.asset(AssetPaths().center_icon),
+            ),
           ),
-        ],
-      ),
+        ),),
+
+        Positioned(
+            bottom: 0.0,
+            left: 0.0,
+            right: 0.0,
+            child:       Container(
+            height: 80,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage(AssetPaths().buttom_navigation_bar),
+                fit: BoxFit.cover,
+              ),
+            ),
+            child:
+            Padding(
+              padding: const EdgeInsets.only(left: 30, right: 30,top: 15),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  _buildNavItem(0, AssetPaths().home_button, AppStrings().home),
+                  _buildNavItem(1, AssetPaths().history_button, AppStrings().history),
+                ],
+              ),
+            )
+            /*Stack(children: [
+
+
+              Positioned(
+                bottom: 0.0,
+                left: 0.0,
+                right: 0.0,
+                child:       Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+
+
+
+                    // Positioned(
+                    //   bottom: 10,
+                    //   left: screenWidth * 0.12,
+                    //   right: screenWidth * 0.12,
+                    //   child: ,
+                    // ),
+                    // Positioned(
+                    //   bottom: 50,
+                    //   child: ,
+                    // ),
+                  ],
+                ),)
+            ],)*/
+
+        ))
+      ],),
     );
+
+
+
   }
 
   Widget _buildNavItem(int index, String asset, String label) {
@@ -47,15 +111,19 @@ class BottomNavigationBarCustom extends StatelessWidget {
     return GestureDetector(
       onTap: () => onTap(index),
       child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
+        mainAxisSize: MainAxisSize.min,
         children: [
-          Image.asset(asset, height: 30, color: isSelected ? Colors.blue : Colors.grey),
+          Image.asset(
+            asset,
+            height: 28,
+            color: isSelected ? Colors.orange : Colors.grey,
+          ),
           const SizedBox(height: 4),
           Text(
             label,
             style: TextStyle(
               fontSize: 12,
-              color: isSelected ? Colors.blue : Colors.grey,
+              color: isSelected ? Colors.orange : Colors.grey,
             ),
           ),
         ],
